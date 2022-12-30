ccbbaf709175ab69969e841bf3ebda7d1d930119
kelly galliani <kelly_92b@hotmail.com>
2022-12-30 11:02:50 -0300

    feat: leitura Page done

--- script/clickTest.js
+++ script/clickTest.js
@@ -12,5 +12,18 @@ arrayButtons.forEach((button)=>{
         if(button.innerText === "Pacientes"){
             window.location.replace("./pages/Pacientes/pacientes.html")
         }
+        if(button.innerText === "Leitura"){
+            window.location.replace("./pages/Leitura/leitura.html")
+        }
     })
 })
+
+const inputProfissional = document.querySelector(".inputName")
+const modal = document.querySelector("dialog")
+const closeModal = document.querySelector(".button_modal")
+
+inputProfissional.addEventListener("click", ()=> modal.showModal())
+closeModal.addEventListener("click", ()=> modal.close())
+
+/* DOM MODAL */
+

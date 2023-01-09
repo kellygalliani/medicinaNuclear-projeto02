93b6f881d66db7239cfc4c73f654650788277643
kelly galliani <kelly_92b@hotmail.com>
2023-01-09 16:39:12 -0300

    fix: routes from homePage

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ arrayButtons.forEach((button)=>{
     button.addEventListener("click", ()=>{
         
         if(button.innerText === "Profissionais"){
-            window.location.replace("/pages/Profissionais/profissionais.html")
+            window.location.replace("./pages/Profissionais/profissionais.html")
         }
         if(button.innerText === "Pacientes"){
-            window.location.replace("/pages/Pacientes/pacientes.html")
+            window.location.replace("./pages/Pacientes/pacientes.html")
         }
         if(button.innerText === "Leitura"){
-            window.location.replace("/pages/Leitura/leitura.html")
+            window.location.replace("./pages/Leitura/leitura.html")
         }
         if(button.innerText === "Relatório de dose"){
-            window.location.replace("/pages/Relatório/relatorio.html")
+            window.location.replace("./pages/Relatório/relatorio.html")
         }
     })
 })
